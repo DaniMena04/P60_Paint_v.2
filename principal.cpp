cc1bd80682f71096f47774fe9fdec9f2fc848afd
DaniMena04 <dmenae@est.ups.edu.ec>
2022-07-08 00:19:32 -0500

    Dibujar lineas, rectangulos y circulos

--- principal.cpp
+++ principal.cpp
@@ -10,6 +10,7 @@
 #include <QColorDialog>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QPoint>
 
 #define DEFAULT_ANCHO 3
 
@@ -42,11 +43,11 @@ Principal::~Principal()
 void Principal::paintEvent(QPaintEvent *event)
 {
     // Crear el painter de la ventana principal
-    ///QPainter painter(this);
+    QPainter painter(this);
     // Dibujar la imagen
-    //painter.drawImage(0, 0, *mImagen);
+    painter.drawImage(0, 0, *mImagen);
     // Acepatr el evento
-    //event->accept();
+    event->accept();
 }
 
 void Principal::mousePressEvent(QMouseEvent *event)
@@ -60,9 +61,12 @@ void Principal::mouseMoveEvent(QMouseEvent *event)
 {
     // Validar si se puede dibujar
     if ( !mPuedeDibujar ) {
+        // aceptar el evento
         event->accept();
+        //salir del metodo
         return;
     }
+
     // Capturar el punto donde se suelta el mouse
     mFinal = event->pos();
     // Crear un pincel y establecer atributos
@@ -74,7 +78,7 @@ void Principal::mouseMoveEvent(QMouseEvent *event)
     mPainter->drawLine(mInicial, mFinal);
     // Mostrar el número de líneas en la barra de estado
     ui->statusbar->showMessage("Número de líneas: " + QString::number(++mNumLineas));
-    // Actualizar la interfaz
+    // Actualizar la interfaz (repintar con paintEvent)
     update();
     // actualizar el punto inicial
     mInicial = mFinal;
@@ -134,3 +138,62 @@ void Principal::on_actionGuardar_triggered()
                                  "No se pudo almacenar la imagen.");
     }
 }
+
+void Principal::on_actionLineas_triggered()
+{
+
+//    qDebug() << "Punto X1: " << mInicial.x();
+//    qDebug() << "Punto Y1: " << mInicial.y();
+//    qDebug() << "Punto X2: " << mFinal.x();
+//    qDebug() << "Punto Y2: " << mFinal.y();
+
+    QPen pincel;
+    pincel.setColor(mColor);
+    pincel.setWidth(mAncho);
+
+    QLine linea(mInicial.x(),mInicial.ry(),mFinal.x(),mFinal.ry());
+
+    mPainter->drawLine(linea);
+
+    update();
+}
+
+
+void Principal::on_actionRect_nculos_triggered()
+{
+    QPen pincel;
+     pincel.setColor(mColor);
+     pincel.setWidth(mAncho);
+     // Dibujar una linea
+     mPainter->setPen(pincel);
+     mPainter->drawRect(mInicial.x(),mInicial.y(),mFinal.x()-mInicial.x(),mFinal.y()-mInicial.y());
+
+//     qDebug()<<"Eje de las x punto inicial: "<<mInicial.x()<<" rs"<<mInicial.rx();
+//     qDebug()<<"Eje de las y punto inicial: "<<mInicial.y()<<" rs"<<mInicial.ry();
+//     qDebug()<<"Eje de las x punto Final: "<<mFinal.x()<<" rs"<<mFinal.rx();
+//     qDebug()<<"Eje de las y punto Final: "<<mFinal.y()<<" rs"<<mFinal.ry();
+     update();
+}
+
+
+void Principal::on_actionCircunferencias_triggered()
+{
+    QPen pincel;
+    pincel.setColor(mColor);
+    pincel.setWidth(mAncho);
+    // Dibujar una linea
+    mPainter->setPen(pincel);
+
+//    mCentro.setX(mInicial.rx());
+//    mCentro.setY(mInicial.ry());
+    mPainter->drawEllipse(mInicial.x(),mInicial.y(),mFinal.x()-mInicial.x(),mFinal.y()-mInicial.y());
+//    mPainter->drawEllipse(mInicial.y(),mFinal.y(),mInicial.y(),mFinal.y());
+//    mPainter->drawEllipse(QRect(0,0,mInicial.y(),mFinal.y()));
+//    mPainter->drawEllipse(100,100,100,100);
+//    qDebug()<<"Eje de las x punto inicial: "<<mInicial.x()<<" rs"<<mInicial.rx();
+//    qDebug()<<"Eje de las y punto inicial: "<<mInicial.y()<<" rs"<<mInicial.ry();
+//    qDebug()<<"Eje de las x punto Final: "<<mFinal.x()<<" rs"<<mFinal.rx();
+//    qDebug()<<"Eje de las y punto Final: "<<mFinal.y()<<" rs"<<mFinal.ry();
+    update();
+}
+
